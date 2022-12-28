1941a729b88fe1e98bb55846adb6c01709c1a69c
oktapascal <oktaiscool@gmail.com>
2022-12-28 16:32:37 +0700

    refactor base private controller
    refactor base private controller

--- server/controllers/BasePrivateController.ts
+++ server/controllers/BasePrivateController.ts
@@ -1,27 +1,14 @@
-import { H3Event } from 'h3'
+import {H3Event} from "h3";
 
 export class BasePrivateController {
-    private _id_user: string
-    private _authenticated: boolean
-
-    constructor(event: H3Event) {
-        this._id_user = event.context.auth.id_user
-        this._authenticated = event.context.auth.authenticated
+    constructor(private event: H3Event) {
     }
 
     get id_user(): string {
-        return this._id_user
-    }
-
-    set id_user(value: string) {
-        this._id_user = value
+        return this.event.context.auth.id_user;
     }
 
     get authenticated(): boolean {
-        return this._authenticated
-    }
-
-    set authenticated(value: boolean) {
-        this._authenticated = value
+        return this.event.context.auth.authenticated;
     }
 }
