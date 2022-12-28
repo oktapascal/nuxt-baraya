5f3dd27db75ddc9dc781688284d81a30639f08ae
oktapascal <oktaiscool@gmail.com>
2022-12-28 16:49:04 +0700

    Install modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,43 +3,45 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  alias: {
-    pinia: '/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs',
-  },
-  app: {
-    head: {
-      bodyAttrs: {
-        class: 'bg-white dark:bg-black transition duration-300',
-      },
-      link: [
-        {
-          rel: 'stylesheet',
-          href: 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css',
-        },
-        {
-          rel: 'icon',
-          type: 'image/x-icon',
-          href: '/favicon.ico',
-        },
-      ],
-      title: 'Barraya App',
+    alias: {
+        pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs",
     },
-  },
-  build: {
-    transpile: ['zod', '@vee-validate/zod', 'vee-validate'],
-  },
-  css: ['~/assets/css/tailwind.css'],
-  modules: [
-    '@vueuse/nuxt',
-    ['@pinia/nuxt', { autoImports: ['defineStore', 'acceptHMRUpdate'] }],
-  ],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+    app: {
+        head: {
+            bodyAttrs: {
+                class: "bg-white dark:bg-black transition duration-300",
+            },
+            link: [
+                {
+                    rel: "stylesheet",
+                    href: "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css",
+                },
+                {
+                    rel: "icon",
+                    type: "image/x-icon",
+                    href: "/favicon.ico",
+                },
+            ],
+            title: "Barraya App",
+        },
     },
-  },
-  typescript: {
-    shim: false,
-  },
+    build: {
+        transpile: ["zod", "@vee-validate/zod", "vee-validate"],
+    },
+    css: ["~/assets/css/tailwind.css"],
+    modules: [
+        "nuxt-api-party",
+        "nuxt-security",
+        "@vueuse/nuxt",
+        ["@pinia/nuxt", {autoImports: ["defineStore", "acceptHMRUpdate"]}],
+    ],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
+    typescript: {
+        shim: false,
+    },
 });
